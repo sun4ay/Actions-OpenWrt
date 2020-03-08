70589c4a8ee6dc072cf713a0049538ffc6696879
a520ass <a520ass@gmail.com>
2020-03-08 16:02:41 +0800

    测试自定义files

--- diy.sh
+++ diy.sh
@@ -7,3 +7,11 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 使用旧版的upnp
+# 删除现有的包
+rm -rf feeds/packages/net/miniupnpd
+rm -rf feeds/luci/applications/luci-app-upnp
+# 使用自定义的包
+cp -rf files/miniupnpd feeds/packages/net/
+cp -rf files/luci-app-upnp feeds/luci/applications/
